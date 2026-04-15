b961b750b099abc8d3c6228c60811fbd53481bd0
bhqn <bernardo_quintanilha@hotmail.com>
2026-04-15 18:25:26 -0300

    final

--- frontend/src/components/popup/components/paymentSucessPopup/PaymentSucessPopup.jsx
+++ frontend/src/components/popup/components/paymentSucessPopup/PaymentSucessPopup.jsx
@@ -1,8 +1,20 @@
+import CheckIcon from "../../../../assets/checkIcon.png"
+import "./PaymentSuccessPopup.css"
+
 export default function PaymentSuccessContent({ onClose }) {
   return (
     <div className="success__content">
-      <h2>Pagamento confirmado 🎉</h2>
-      <p>Seu pedido foi realizado com sucesso!</p>
+      <div className="payment-success__icon" aria-hidden="true">
+        <img className= "payment-success__logo"src={CheckIcon}/>
+      </div>
+
+      <p className="payment-success__badge">Pagamento confirmado</p>
+      <h2 className="payment-success__title">Pedido realizado!</h2>
+      <p className="payment-success__description">
+        Seu pagamento foi processado com sucesso.
+        <br />
+        Em breve você receberá a confirmação por e-mail.
+      </p>
 
       <button onClick={onClose}>Fechar</button>
     </div>
